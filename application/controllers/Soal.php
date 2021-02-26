37b8668bd5bca56d25ab4f339b8b44b1f574953c
arfanravy <erst00.00@gmail.com>
2021-02-26 17:37:06 +0700

    ravy

--- application/controllers/Soal.php
+++ application/controllers/Soal.php
@@ -24,11 +24,172 @@ class Soal extends CI_Controller
   public function __construct()
   {
     parent::__construct();
+    check_not_login();
+    $this->load->model('Soal_model', 'soal');
+    $this->load->model('Paket_model', 'paket');
   }
 
-  public function index()
+  public function index($id = NULL)
   {
+    $data['paket'] = $this->paket->get($id)->row();
+    // var_dump($data['paket']);
+    // die;
+    $data['soal'] = $this->soal->get(null, $data['paket']->id_paket)->row();
+  // var_dump($data['soal']);
+  //   die;
+    $this->template->load('template', 'tes/soal',$data);
+
     // 
+  }
+
+  public function getAjax($id)
+  {
+    $list = $this->soal->getDataTables($id);
+    $data = [];
+    $no = @$_POST['start'];
+    foreach ($list as $soal) {
+      $no++;
+      $row = [];
+      $row[] = $no . '.';
+      $row[] = $soal->soal_text;
+      $row[] = $soal->soal_gambar != null ? '<img src="' . site_url('uploads/soal/') . $soal->soal_gambar . '"  class="rounded mx-auto d-block" width="200px">' : '<img src="' . site_url('assets/able/assets/images/') . 'default.png" class="rounded mx-auto d-block" width="200px">';
+      $row[] = $soal->soal_suara;
+      $row[] = $soal->option_a;
+      $row[] = $soal->option_b;
+      $row[] = $soal->option_c;
+      $row[] = $soal->option_d;
+      $row[] = $soal->option_e;
+      $row[] = $soal->jawaban_benar;
+      
+      // $row[] = $soal->created;
+      // $row[] = $soal->updated;
+      $row[] = '
+          <button type="button" value="' . $soal->id_soal . '" class="btn btn-success has-ripple update"><i class="feather mr-2 icon-edit"></i>Update<span class="ripple ripple-animate" style="height: 112.65px; width: 112.65px; animation-duration: 0.7s; animation-timing-function: linear; background: rgb(255, 255, 255) none repeat scroll 0% 0%; opacity: 0.4; top: -38.825px; left: -2.85833px;"></span></button>
+          <button type="button" value="' . $soal->id_soal . '" class="btn btn-danger has-ripple delete"><i class="feather mr-2 icon-trash"></i>Delete<span class="ripple ripple-animate" style="height: 112.65px; width: 112.65px; animation-duration: 0.7s; animation-timing-function: linear; background: rgb(255, 255, 255) none repeat scroll 0% 0%; opacity: 0.4; top: -38.825px; left: -2.85833px;"></span></button>
+      ';
+
+      $data[] = $row;
+    }
+    $output = [
+      'draw' => @$_POST['draw'],
+      'recordsTotal' => $this->soal->countAll(),
+      'recordsFiltered' => $this->soal->countFiltered($id),
+      'data' => $data
+    ];
+    echo json_encode($output);
+  }
+
+  public function get()
+  {
+    $get = $this->input->get(null, TRUE);
+    $query = $this->soal->get($get['id']);
+    echo json_encode($query->row());
+  }
+
+  public function add()
+  {
+    $post = $this->input->post(null, TRUE);
+
+    $this->form_validation->set_rules('soal_text', 'soal_text', 'required');
+    $this->form_validation->set_rules('paket_id', 'paket_id', 'required');
+    $this->form_validation->set_rules('option_a', 'option_a', 'required');
+    $this->form_validation->set_rules('option_b', 'option_b', 'required');
+    $this->form_validation->set_rules('option_c', 'option_c', 'required');
+    $this->form_validation->set_rules('option_d', 'option_d', 'required');
+    $this->form_validation->set_rules('option_e', 'option_e', 'required');
+    $this->form_validation->set_rules('jawaban_benar', 'jawaban_benar', 'required');
+    $this->form_validation->set_message('required', '%s masih kosong, silahkan isi');
+
+    if ($this->form_validation->run() == false) {
+      echo json_encode(validation_errors());
+    } else {
+      
+      $config['upload_path']    = './uploads/soal/';
+      $config['allowed_types']  = 'gif|png|jpg|jpeg';
+      $config['max_size']       = 2048;
+      $config['file_name']       = "soal-" . date('ymd') . '-' . substr(md5(rand()), 0, 10);
+
+      $this->load->library('upload', $config);
+
+      if (@$_FILES['soal_gambar']['name'] != null) {
+        if ($this->upload->do_upload('soal_gambar')) {
+          $post['soal_gambar'] = $this->upload->data('file_name');
+          $data = $this->soal->create($post);
+          echo json_encode($data);
+        } else {
+          $error = $this->upload->display_errors();
+          echo json_encode($error);
+        }
+      } else {
+        $post['soal_gambar'] = null;
+        $data = $this->soal->create($post);
+        echo json_encode($data);
+      }
+    }
+  }
+ 
+
+  public function update()
+  {
+    $post = $this->input->post(null, TRUE);
+
+    $this->form_validation->set_rules('soal_text', 'soal_text', 'required');
+    $this->form_validation->set_rules('option_a', 'option_a', 'required');
+    $this->form_validation->set_rules('option_b', 'option_b', 'required');
+    $this->form_validation->set_rules('option_c', 'option_c', 'required');
+    $this->form_validation->set_rules('option_d', 'option_d', 'required');
+    $this->form_validation->set_rules('option_e', 'option_e', 'required');
+    $this->form_validation->set_rules('jawaban_benar', 'jawaban_benar', 'required');
+    $this->form_validation->set_message('required', '%s masih kosong, silahkan isi');
+
+    if ($this->form_validation->run() == false) {
+      echo json_encode(validation_errors());
+    } else {
+     
+      $config['upload_path']    = './uploads/soal/';
+      $config['allowed_types']  = 'gif|png|jpg|jpeg';
+      $config['max_size']       = 204800;
+      $config['file_name']       = "soal-" . date('ymd') . '-' . substr(md5(rand()), 0, 10);
+
+      $this->load->library('upload', $config);
+
+      if (@$_FILES['soal_gambar']['name'] != null) {
+        if ($this->upload->do_upload('soal_gambar')) {
+          $soal = $this->soal->get($post['id_soal'])->row();
+
+          if ($soal->soal_gambar != null) {
+            $target_file = './uploads/soal/' . $soal->soal_gambar;
+            unlink($target_file);
+          }
+
+
+          $post['soal_gambar'] = $this->upload->data('file_name');
+          $data = $this->soal->update($post);
+          echo json_encode($data);
+        } else {
+          $error = $this->upload->display_errors();
+          echo json_encode($error);
+        }
+      } else {
+        $post['soal_gambar'] = null;
+        $data = $this->soal->update($post);
+        echo json_encode($data);
+      }
+    }
+  }
+
+  public function delete()
+  {
+    $post = $this->input->post(null, TRUE);
+    $soal = $this->soal->get($post['id'])->row();
+
+    if ($soal->soal_gambar != null) {
+      $target_file = './uploads/soal/' . $soal->soal_gambar;
+      unlink($target_file);
+    }
+
+    $data = $this->soal->delete($post['id']);
+    echo json_encode($data);
   }
 }
 
--- application/models/Soal_model.php
+++ application/models/Soal_model.php
@@ -19,20 +19,132 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Soal_model extends CI_Model
 {
 
+  var $column_order = [null, 'soal_text'];
+  var $column_search = ['soal_text'];
+
+  var $order = ['id_soal' => 'asc'];
   // ------------------------------------------------------------------------
 
-  public function __construct()
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+
+  public function _getDataTablesQuery($id)
   {
-    parent::__construct();
+    $this->db->select("tb_soal.*, tb_paket.id_paket");
+    $this->db->from("tb_soal");
+    $this->db->join("tb_paket", "tb_paket.id_paket = tb_soal.paket_id");
+    $this->db->where('paket_id', $id);
+
+
+    $i = 0;
+
+    foreach ($this->column_search as $item) {
+      if (@$_POST['search']['value']) {
+        if ($i == 0) {
+          $this->db->group_start();
+          $this->db->like($item, $_POST['search']['value']);
+        } else {
+          $this->db->or_like($item, $_POST['search']['value']);
+        }
+        if (count($this->column_search) - 1 == $i) {
+          $this->db->group_end();
+        }
+      }
+      $i++;
+    }
+    if (isset($_POST['order'])) {
+      $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+    } elseif (isset($this->order)) {
+      $order = $this->order;
+      $this->db->order_by(key($order), $order[key($order)]);
+    }
   }
 
-  // ------------------------------------------------------------------------
-
-
-  // ------------------------------------------------------------------------
-  public function index()
+  public function getDataTables($id)
   {
-    // 
+    $this->_getDataTablesQuery($id);
+
+    if (@$_POST['length'] != -1)
+      $this->db->limit(@$_POST['length'], @$_POST['start']);
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function countFiltered($id)
+  {
+    $this->_getDataTablesQuery($id);
+    $query = $this->db->get();
+    return $query->num_rows();
+  }
+
+  public function countAll()
+  {
+    $this->db->from('tb_soal');
+    return $this->db->count_all_results();
+  }
+
+  public function get($id = null,$id_paket = null)
+  {
+    $this->db->from('tb_soal');
+    if ($id != null) {
+      $this->db->where('id_soal', $id);
+    }
+    if ($id_paket != null) {
+      $this->db->where('paket_id', $id_paket);
+    }
+    $query = $this->db->get();
+    return $query;
+  }
+
+  public function create($post)
+  {
+    $params['paket_id'] = htmlspecialchars($post['paket_id']);
+    $params['soal_text'] = htmlspecialchars($post['soal_text']);
+    $params['soal_gambar'] = htmlspecialchars($post['soal_gambar']);
+    // $params['soal_suara'] = htmlspecialchars($post['soal_suara']);
+    $params['option_a'] = htmlspecialchars($post['option_a']);
+    $params['option_b'] = htmlspecialchars($post['option_b']);
+    $params['option_c'] = htmlspecialchars($post['option_c']);
+    $params['option_d'] = htmlspecialchars($post['option_d']);
+    $params['option_e'] = htmlspecialchars($post['option_e']);
+    $params['jawaban_benar'] = htmlspecialchars($post['jawaban_benar']);
+
+    $this->db->insert('tb_soal', $params);
+    return $this->db->affected_rows();
+  }
+
+  public function update($post)
+  {
+    $params['paket_id'] = htmlspecialchars($post['paket_id']);
+    $params['soal_text'] = htmlspecialchars($post['soal_text']);
+   if  ($post['soal_gambar'] !=null)
+   {
+    $params['soal_gambar'] = htmlspecialchars($post['soal_gambar']);
+   }
+  
+    // $params['soal_suara'] = htmlspecialchars($post['soal_suara']);
+    $params['option_a'] = htmlspecialchars($post['option_a']);
+    $params['option_b'] = htmlspecialchars($post['option_b']);
+    $params['option_c'] = htmlspecialchars($post['option_c']);
+    $params['option_d'] = htmlspecialchars($post['option_d']);
+    $params['option_e'] = htmlspecialchars($post['option_e']);
+    $params['jawaban_benar'] = htmlspecialchars($post['jawaban_benar']);
+
+    
+    $params['updated'] = date('Y-m-d H:i:s');
+
+    $this->db->where('id_soal', $post['id_soal']);
+    $this->db->update('tb_soal', $params);
+    return $this->db->affected_rows();
+  }
+
+  public function delete($id)
+  {
+    $this->db->where('id_soal', $id);
+    $this->db->delete('tb_soal');
+    return $this->db->affected_rows();
   }
 
   // ------------------------------------------------------------------------
