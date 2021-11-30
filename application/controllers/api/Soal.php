8c28eb9b078f4e474538883355f9e00518c25102
khisan <khisan8@gmail.com>
2021-11-30 09:19:54 +0700

    revision convert html tags to just string value

--- application/controllers/api/Soal.php
+++ application/controllers/api/Soal.php
@@ -14,10 +14,24 @@ class Soal extends BD_Controller
   public function index_get($id = null, $id_paket = null, $id_mapel = null)
   {
     $query = $this->soal->getRest($id, $id_paket, $id_mapel);
+    $data = [];
+    foreach ($query as $res) {
+      $row['id_soal'] = $res->id_soal;
+      $row['paket_id'] = $res->paket_id;
+      $row['mapel_id'] = $res->mapel_id;
+      $row['soal'] = strip_tags(html_entity_decode(str_replace("\n", "", $res->soal)));
+      $row['option_a'] = strip_tags(html_entity_decode(str_replace("\n", "", $res->option_a)));
+      $row['option_b'] = strip_tags(html_entity_decode(str_replace("\n", "", $res->option_b)));
+      $row['option_c'] = strip_tags(html_entity_decode(str_replace("\n", "", $res->option_c)));
+      $row['option_d'] = strip_tags(html_entity_decode(str_replace("\n", "", $res->option_d)));
+      $row['option_e'] = strip_tags(html_entity_decode(str_replace("\n", "", $res->option_e)));
+      $row['jawaban_benar'] = $res->jawaban_benar;
+      $data[] = $row;
+    }
     $this->set_response([
       'status' => REST_Controller::HTTP_OK,
       'message' => "Get Data Soal Berhasil",
-      'data' => $query,
+      'data' => $data,
     ]);
   }
 }
